c79b493b5f2cb10e7d99b49935b6cba225c5b9bc
August Fisherman <fshrmn.august@gmail.com>
2026-01-13 19:40:02 +0300

    routing

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	m := http.NewServeMux()
+
+	m.HandleFunc("/", IndexHandler)
+	m.HandleFunc("/tasks", TasksHandler)
+	m.HandleFunc("/create_task", CreateTaskhandler)
+	log.Println("Server started...")
+	http.ListenAndServe(":8080", m)
+
+}
