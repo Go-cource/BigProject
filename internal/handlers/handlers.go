52c0e46c4fa713505136d5486b36b593ca224fda
August Fisherman <fshrmn.august@gmail.com>
2026-01-13 20:03:04 +0300

    handlers started

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,15 +1,30 @@
 package handlers
 
-import "net/http"
+import (
+	"html/template"
+	"net/http"
+)
 
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
-
+	if r.Method == http.MethodGet {
+		comps := SelectAllComps()
+		tmpl, _ := template.ParseFiles("../web/templates/index.html")
+		tmpl.Execute(w, comps)
+	}
 }
 
 func TasksHandler(w http.ResponseWriter, r *http.Request) {
-
+	if r.Method == http.MethodGet {
+		tasks := SelectAllTasks()
+		tmpl, _ := template.ParseFiles("../web/templates/tasks.html")
+		tmpl.Execute(w, tasks)
+	}
 }
 
 func CreateTaskhandler(w http.ResponseWriter, r *http.Request) {
-
+	if r.Method == http.MethodGet {
+		comps := SelectAllComps()
+		tmpl, _ := template.ParseFiles("../web/templates/createTask.html")
+		tmpl.Execute(w, comps)
+	}
 }
