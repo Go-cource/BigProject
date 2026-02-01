a630ea4d98f53c180e4360a5874c948c1afb234b
August Fisherman <fshrmn.august@gmail.com>
2026-02-01 13:06:12 +0300

    client result

--- client.go
+++ client.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"os/exec"
 )
 
 const addr = "http://localhost:8080"
 const CompName = "Comp1"
+
+type CompTaskResult struct {
+	TaskId     int
+	TaskResult string
+}
 
 type Task struct {
 	TaskId           int
@@ -33,6 +40,51 @@ func getMyTasks(path string) []Task {
 	return tasks
 }
 
+func executeCommand(command string, args ...string) (string, error) {
+	cmd := exec.Command(command, args...)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return "", fmt.Errorf("ошибка выполнения команды: %w", err)
+	}
+	return string(output), nil
+}
+
+func execMyTasks(tasks []Task) error {
+	if len(tasks) == 0 {
+		return nil
+	}
+	for _, v := range tasks {
+		taskRes := CompTaskResult{}
+		taskRes.TaskId = v.TaskId
+		tr, err := executeCommand(v.TaskText)
+		if err != nil {
+			taskRes.TaskResult = "error while executing"
+		} else {
+			taskRes.TaskResult = tr
+		}
+		finalRes, err := json.Marshal(taskRes)
+		if err != nil {
+			fmt.Println(err)
+			return err
+		}
+		postRes, err := http.Post(addr+"/tasks", "application/json", bytes.NewBuffer(finalRes))
+		if err != nil {
+			fmt.Println(err)
+			return err
+		}
+		srvAnswer := map[string]interface{}{}
+		err = json.NewDecoder(postRes.Body).Decode(&srvAnswer)
+		if srvAnswer["status"] == "ok" {
+			return nil
+		}
+	}
+	return nil
+}
+
 func main() {
-	fmt.Println(getMyTasks("/ask_tasks?name=" + CompName))
+	// fmt.Println(getMyTasks("/ask_tasks?name=" + CompName))
+	err := execMyTasks(getMyTasks("/ask_tasks?name=" + CompName))
+	if err != nil {
+		fmt.Println(err)
+	}
 }
